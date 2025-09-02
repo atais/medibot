df696be94b8d362fdc0ba383f2c0fd0ee1b2e05f
atais <atais.jr@gmail.com>
2025-09-02 16:17:13 +0200

    simplify user_context, cleanup requirements.txt

--- user_context.py
+++ user_context.py
@@ -1,10 +1,11 @@
 import logging
+import uuid
 
 import requests
+from fake_useragent import UserAgent
 from requests.adapters import HTTPAdapter
 
 import medicover
-from user_data import UserData
 
 _default_headers = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
@@ -21,53 +22,55 @@ class UserContext(HTTPAdapter):
         self.username = username
         self.password = password
 
+        self.user_agent: str = UserAgent().random
+        self.device_id: str = str(uuid.uuid4())
+        self.bearer_token: str = ""
+        self.refresh_token: str = ""
+
         self.session = requests.Session()
         self.session.headers.update(_default_headers)
         self.session.mount('https://', self)
         self.session.mount('http://', self)
-
-        self.user_data = UserData(username)
-        self.session.headers["User-Agent"] = self.user_data.user_agent
-        self._update_bearer()
+        self.session.headers["User-Agent"] = self.user_agent
 
         super().__init__(*args, **kwargs)
 
-    def _update_bearer(self) -> None:
-        self.session.headers["authorization"] = "Bearer " + self.user_data.bearer_token
 
     def _login(self) -> None:
         logging.info(f"Logging in {self.username}")
-        r = medicover.login(self.username, self.password, self.user_data.device_id, self.session)
-        self.user_data.update_tokens(*r)
-        self._update_bearer()
+        at, rt = medicover.login(self.username, self.password, self.device_id, self.session)
+        self.bearer_token = at
+        self.refresh_token = rt
+        self.session.headers["authorization"] = "Bearer " + self.bearer_token
 
     def _refresh(self) -> None:
         logging.info(f"Keep alive {self.username}")
-        r = medicover.refresh(self.user_data.refresh_token, self.session)
-        self.user_data.update_tokens(*r)
-        self._update_bearer()
+        at, rt = medicover.refresh(self.refresh_token, self.session)
+        self.bearer_token = at
+        self.refresh_token = rt
+        self.session.headers["authorization"] = "Bearer " + self.bearer_token
 
     def send(self, request, **kwargs):
         response = super().send(request, **kwargs)
 
         if response.status_code == 401:
             try:
-                if self.user_data.bearer_token != "":
-                    logging.warning(f"401 {self.user_data.username} no #1, trying to reload token")
+                if self.bearer_token != "":
+                    logging.warning(f"401 {self.username} no #1, trying to reload token")
                     self._refresh()
-                    request.headers["authorization"] = "Bearer " + self.user_data.bearer_token
+                    request.headers["authorization"] = "Bearer " + self.bearer_token
                     response = super().send(request, **kwargs)
                 else:
-                    logging.warning(f"401 {self.user_data.username} no #1, its not possible to reload token")
+                    logging.warning(f"401 {self.username} no #1, its not possible to reload token")
             except TypeError:
                 pass
             finally:
                 if response.status_code == 401:
-                    logging.warning(f"401 {self.user_data.username} no #2, trying to login")
+                    logging.warning(f"401 {self.username} no #2, trying to login")
                     self._login()
-                    request.headers["authorization"] = "Bearer " + self.user_data.bearer_token
+                    request.headers["authorization"] = "Bearer " + self.bearer_token
                     response = super().send(request, **kwargs)
                     if response.status_code == 401:
-                        raise Exception(f"401 {self.user_data.username} no #3, there is some issue with your account")
+                        raise Exception(f"401 {self.username} no #3, there is some issue with your account")
 
         return response
