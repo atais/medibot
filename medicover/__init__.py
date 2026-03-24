1988d8201b7bdc75f4d3512e94693b8ea4f7c9e8
Michał Siatkowski <atais.jr@gmail.com>
2026-03-24 09:58:57 +0100

    fixes

--- medicover/__init__.py
+++ medicover/__init__.py
@@ -1,6 +1,21 @@
-from ._constants import API, LOGIN, ONLINE24  # noqa: F401
-from .appointments import get_slots, get_filters, get_person_appointments  # noqa: F401
-from .auth import login, refresh  # noqa: F401
-from .book import book, delete  # noqa: F401
-from .personal_data import personal_data  # noqa: F401
-from .referrals import get_referrals  # noqa: F401
+from ._constants import API, LOGIN, ONLINE24
+from .appointments import get_slots, get_filters, get_person_appointments
+from .auth import login, refresh
+from .book import book, delete
+from .personal_data import personal_data
+from .referrals import get_referrals
+
+__all__ = [
+    "API",
+    "LOGIN",
+    "ONLINE24",
+    "get_slots",
+    "get_filters",
+    "get_person_appointments",
+    "login",
+    "refresh",
+    "book",
+    "delete",
+    "personal_data",
+    "get_referrals",
+]
